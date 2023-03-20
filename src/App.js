21cd742696caffe9491129a5d4ecd438d938335b
Ankit Kumar <ankit.kumar@financialforce.com>
2023-03-20 11:03:07 +0530

    Checking the jenkins job

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jenkins Intermediate Goal
+          Testing jenkins job
         </p>
         <a
           className="App-link"
